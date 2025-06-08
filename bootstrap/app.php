abfd1bec873a453d097172e5da6b8d91ab81dbe4
dedy priyatna <57609978+dedypry@users.noreply.github.com>
2025-06-08 14:52:29 +0700

    upda

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,9 +6,6 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets;
-use Illuminate\Http\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Inertia\Inertia;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -32,17 +29,5 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
-    $exceptions->respond(function (Response $response, Throwable $exception, Request $request) {
-        if (! app()->environment(['local', 'testing']) && in_array($response->getStatusCode(), [500, 503, 404, 403])) {
-            return Inertia::render('ErrorPage', ['status' => $response->getStatusCode()])
-                ->toResponse($request)
-                ->setStatusCode($response->getStatusCode());
-        } elseif ($response->getStatusCode() === 419) {
-            return back()->with([
-                'message' => 'The page expired, please try again.',
-            ]);
-        }
-
-        return $response;
-    });
+        //
     })->create();
