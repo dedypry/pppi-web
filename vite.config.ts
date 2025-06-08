3152dbc5e7de7fe974220db538b25c0f1e07a6e6
dedy priyatna <57609978+dedypry@users.noreply.github.com>
2025-06-08 14:17:19 +0700

    update

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,12 @@ import react from '@vitejs/plugin-react';
 import laravel from 'laravel-vite-plugin';
 import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
+import { globSync } from 'glob';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.tsx'],
+            input: ['resources/css/app.css', 'resources/js/app.tsx', ...globSync('resources/js/pages/**/*.tsx')],
             ssr: 'resources/js/ssr.tsx',
             refresh: true,
         }),
