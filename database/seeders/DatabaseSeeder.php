2bcb34a0e644d0d496b24e0cdd7a3938545bdfaa
dedy priyatna <57609978+dedypry@users.noreply.github.com>
2025-06-08 13:29:46 +0700

    update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,11 +16,19 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        // User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'admin@gmail.com',
-        //     'password' => bcrypt('admin123')
-        // ]);
+        $user = User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'password' => bcrypt('admin123')
+        ]);
+        $user->profile()->create([
+            "gender" => 'male',
+            "citizenship" => 'wni'
+        ]);
+
+        $role = Role::findOrCreate('super_admin', 'web');
+
+        $user->assignRole($role);
 
         $this->call(ProvinceSeeder::class);
         $this->call(CitySeeder::class);
