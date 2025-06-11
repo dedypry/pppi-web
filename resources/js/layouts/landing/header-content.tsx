f9289420268517687ecb23ec748cd09fc37ed673
dedy priyatna <57609978+dedypry@users.noreply.github.com>
2025-06-11 13:21:26 +0700

    update

--- resources/js/layouts/landing/header-content.tsx
+++ resources/js/layouts/landing/header-content.tsx
@@ -2,13 +2,12 @@ import { ReactNode } from 'react';
 
 interface Props {
     children?: ReactNode;
-    height?: string;
     title?: string;
     subtitle?: string;
 }
-export default function HeaderContent({ children, height = '200px', title, subtitle }: Props) {
+export default function HeaderContent({ children, title, subtitle }: Props) {
     return (
-        <div className={`-mt-[60px] mb-10 shadow-lg h-[${height}] flex w-full items-end pb-5 bg-gradient-to-b from-primary-900 to-primary-700`}>
+        <div className={`-mt-[60px] mb-10 shadow-lg h-[200px] flex w-full items-end pb-5 bg-gradient-to-b from-primary-900 to-primary-700`}>
             {children}
             <div className="container mx-auto flex flex-col gap-2">
                 <p className="text-white text-[40px] font-bold">{title}</p>
